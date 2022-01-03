cbb61ad9431995756be882336e7c8aa01ffd6d8a
Rukin Danil <Rukin2018D@yandex.ru>
2022-01-03 12:25:56 +0700

    Определение интерфейса синтаксического анализатора

---
+++ Compiler/ISyntaxAnalyzer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler
+{
+	public interface ISyntaxAnalyzer
+	{
+		void Program();
+		void ClassDescription();
+		void Descriptions();
+		void DataDescription();
+		void NamedConstant();
+		void Data();
+		void FunctionDescription();
+		void Operator();
+		void CompoundOperator();
+		void CompoundOperatorBody();
+		void SimpleOperator();
+		void AssignmentOperator();
+		void Name();
+		void WhileCycle();
+		void FunctionCall();
+		void Expression();
+		void FirstLevel();
+		void SecondLevel();
+		void ThirdLevel();
+		void FourthLevel();
+		void FifthLevel();
+	}
+}
