402425e4fa8924a8013b6a879f1296c82bba03f5
Rukin Danil <Rukin2018D@yandex.ru>
2022-01-09 18:35:21 +0700

    Семантические типы

---
+++ Compiler/SemanticTypes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler
+{
+	public enum  SemanticTypes
+	{
+		Variable,  // a, b, c
+		VariableType,  // int, double
+
+		ClassObject,   // Human h = new Human(); // h is object
+		ClassType,   // Human is type
+
+		Function,
+
+		Constant,  // const int a = 100; // a is constant
+
+		Struct,
+		StructType,
+
+		ArrayObject,  // int[] a; // a is object
+		ArrayType,  // int[], double[], ...
+
+		Label  // start:
+	}
+}
